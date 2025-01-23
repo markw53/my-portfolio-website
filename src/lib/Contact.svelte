7fd96646b4cd38dd4d693922d7c4de90c50328a9
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2025-01-23 15:46:54 +0000

    update contact form to use netlify handling

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -2,25 +2,15 @@
 	let name = '';
 	let email = '';
 	let message = '';
-	let status = '';
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		name = '';
-		email = '';
-		message = '';
-		status = 'Form submitted successfully!';
-	};
 </script>
 
 <section class="bg-neutral-white text-primary mb-4 rounded-sm p-6">
 	<h2 class="mb-6 text-4xl font-semibold">Contact</h2>
 
 	<form name="contact" method="POST" data-netlify="true">
-		<!-- Hidden input to tell Netlify what form it is -->
 		<input type="hidden" name="form-name" value="contact" />
 
-		<!-- Name Field -->
 		<label for="name" class="text-primary block font-semibold">Name</label>
 		<input
 			type="text"
@@ -32,7 +22,6 @@
 			class="focus:ring-secondary mt-1 block w-full rounded-md border border-gray-300 p-3 text-gray-900 shadow-sm focus:outline-none focus:ring-2"
 		/>
 
-		<!-- Email Field -->
 		<label for="email" class="text-primary mt-4 block font-semibold">Email</label>
 		<input
 			type="email"
@@ -44,7 +33,6 @@
 			class="focus:ring-secondary mt-1 block w-full rounded-md border border-gray-300 p-3 text-black text-gray-900 shadow-sm focus:outline-none focus:ring-2"
 		/>
 
-		<!-- Message Field -->
 		<label for="message" class="text-primary mt-4 block font-semibold">Message</label>
 		<textarea
 			id="message"
@@ -61,11 +49,6 @@
 		>
 			Submit
 		</button>
-		<!--
-		{#if status}
-			<p class="mt-4 text-center text-green-600">{status}</p>
-		{/if}
-	-->
 	</form>
 </section>
 
