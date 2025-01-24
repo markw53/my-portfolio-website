c19efc0062ca7ae5edcf15cb56a8f6cf8a90cc42
kwildeDev <73887333+kwildeDev@users.noreply.github.com>
2025-01-24 09:51:52 +0000

    working on fixing netlify form

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -2,42 +2,16 @@
 	let name = '';
 	let email = '';
 	let message = '';
-
 </script>
 
-
-<section class="bg-neutral-white text-primary mb-4 rounded-sm p-6">
+<section class="mb-4 rounded-sm bg-neutral-white p-6 text-primary">
 	<h2 class="mb-6 text-4xl font-semibold">Contact</h2>
 
 	<form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
 		<input type="hidden" name="form-name" value="contact" />
-		<p>
-		  <label>Your Name: <input type="text" name="name" /></label>
-		</p>
-		<p>
-		  <label>Your Email: <input type="email" name="email" /></label>
-		</p>
-		<p>
-		  <label>Your Role: <select name="role[]" multiple>
-			<option value="leader">Leader</option>
-			<option value="follower">Follower</option>
-		  </select></label>
-		</p>
-		<p>
-		  <label>Message: <textarea name="message"></textarea></label>
-		</p>
-		<p>
-		  <button type="submit">Send</button>
-		</p>
-	  </form>
-	  
-
-
-<!--
-	<form name="contact" method="POST" data-netlify="true" action="/">
 		<input type="hidden" name="form-name" value="contact" />
 
-		<label for="name" class="text-primary block font-semibold">Name</label>
+		<label for="name" class="block font-semibold text-primary">Name</label>
 		<input
 			type="text"
 			id="name"
@@ -45,10 +19,10 @@
 			bind:value={name}
 			required
 			placeholder="Your Name"
-			class="focus:ring-secondary mt-1 block w-full rounded-md border border-gray-300 p-3 text-gray-900 shadow-sm focus:outline-none focus:ring-2"
+			class="mt-1 block w-full rounded-md border border-gray-300 p-3 text-gray-900 shadow-sm focus:outline-none focus:ring-2 focus:ring-secondary"
 		/>
 
-		<label for="email" class="text-primary mt-4 block font-semibold">Email</label>
+		<label for="email" class="mt-4 block font-semibold text-primary">Email</label>
 		<input
 			type="email"
 			id="email"
@@ -56,27 +30,27 @@
 			bind:value={email}
 			required
 			placeholder="Your Email"
-			class="focus:ring-secondary mt-1 block w-full rounded-md border border-gray-300 p-3 text-black text-gray-900 shadow-sm focus:outline-none focus:ring-2"
+			class="mt-1 block w-full rounded-md border border-gray-300 p-3 text-black text-gray-900 shadow-sm focus:outline-none focus:ring-2 focus:ring-secondary"
 		/>
 
-		<label for="message" class="text-primary mt-4 block font-semibold">Message</label>
+		<label for="message" class="mt-4 block font-semibold text-primary">Message</label>
 		<textarea
 			id="message"
 			name="message"
 			bind:value={message}
 			required
 			placeholder="Your Message"
-			class="focus:ring-secondary mt-1 block w-full rounded-md border border-gray-300 p-3 text-gray-900 shadow-sm focus:outline-none focus:ring-2"
+			class="mt-1 block w-full rounded-md border border-gray-300 p-3 text-gray-900 shadow-sm focus:outline-none focus:ring-2 focus:ring-secondary"
 		></textarea>
 
 		<button
 			type="submit"
-			class="bg-secondary text-primary hover:bg-secondary-light focus:ring-secondary mt-4 w-full rounded-md py-3 font-semibold shadow-md focus:outline-none focus:ring-2"
+			class="mt-4 w-full rounded-md bg-secondary py-3 font-semibold text-primary shadow-md hover:bg-secondary-light focus:outline-none focus:ring-2 focus:ring-secondary"
 		>
 			Submit
 		</button>
 	</form>
--->
-	</section>
+</section>
+
 <style>
 </style>
